ff3d7e11658ce934abd5c3c092233621c6635d5b
Nhật Lê Đình <nhat.ledinh@ncc.asia>
2026-01-22 16:24:26 +0700

    Add cors package

--- server.js
+++ server.js
@@ -4,19 +4,12 @@ const router = jsonServer.router("database.json");
 const middlewares = jsonServer.defaults();
 const cors = require("cors");
 
-// 👉 ENABLE CORS
 server.use(cors());
-
-// 👉 Default middlewares (logger, static, cors, no-cache)
 server.use(middlewares);
-
-// 👉 Body parser (quan trọng với POST)
 server.use(jsonServer.bodyParser);
 
-// 👉 API routes
 server.use("/api", router);
 
-// 👉 PORT cho Render
 const PORT = process.env.PORT || 3002;
 server.listen(PORT, () => {
   console.log("JSON Server running on port", PORT);
